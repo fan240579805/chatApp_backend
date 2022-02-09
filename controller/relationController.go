918707a28bfdb8ce53ee85de718713285750e029
lucafan <lucafan@tencent.com>
2022-02-09 16:58:09 +0800

    feat: 拉黑好友功能

--- controller/relationController.go
+++ controller/relationController.go
@@ -21,6 +21,7 @@ type Friend struct {
 }
 
 // AddFriendReq from用户像to用户发起好友请求（to还没同意）
+// 此时的 from to是明确的
 func AddFriendReq(c *gin.Context) {
 	var params addFriendReqParams
 	bindErr := c.ShouldBindJSON(&params)
@@ -46,6 +47,7 @@ func AddFriendReq(c *gin.Context) {
 }
 
 // AcceptFriendReq to接受from用户发起的好友请求，将 status 设为1
+// 此时的 from to 是明确的
 func AcceptFriendReq(c *gin.Context) {
 	var relation model.Relation
 	c.ShouldBindJSON(&relation)
@@ -60,7 +62,7 @@ func AcceptFriendReq(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"code": 200,
 			"msg":  "接受成功",
-			"data": "新的好友列表给接受请求的ToUser",
+			"data": FormatFriendList(relation.To, c),
 		})
 		// pushFromUser 将新的好友列表推送给发起好友请求的FromUser
 
@@ -69,17 +71,15 @@ func AcceptFriendReq(c *gin.Context) {
 
 // GetFriendList 获取好友列表
 func GetFriendList(c *gin.Context) {
-	userid, _ := c.Get("userID")
-	relationList, err := model.SelectFriends(userid.(string))
-	if err != nil {
-		log.Println(err.Error())
+	userid, ok := c.Get("userID")
+	if !ok {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{
 			"code": 2003,
-			"msg":  "获取好友列表失败",
+			"msg":  "auth中间件获取userid失败",
 		})
 	} else {
 		// 处理一下friendList并返回给前端
-		friendList := FormatFriendList(relationList, userid.(string))
+		friendList := FormatFriendList(userid.(string), c)
 		c.JSON(http.StatusOK, gin.H{
 			"code": 200,
 			"msg":  "获取好友列表成功",
@@ -94,7 +94,7 @@ func DeleteFriendReq(c *gin.Context) {
 	// 删除好友时并不知道到底 发起删除的用户是from还是被删除人是from
 	var unknownRecord model.Relation
 	c.ShouldBindJSON(&unknownRecord)
-	// 先判断一下
+	// 先判断一下， unknownRecord.From: 主动发起删除的用户
 	rightRelation, _ := model.GetRightRelationRecord(unknownRecord.From, unknownRecord.To)
 	if unknownRecord.From == rightRelation.From {
 		// 发起删除的用户就是  发起好友请求的人（from） status = 2
@@ -108,7 +108,7 @@ func DeleteFriendReq(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"code": 200,
 				"msg":  "删除成功",
-				"data": "新的好友列表给发起删除的FromUser",
+				"data": FormatFriendList(rightRelation.From, c),
 			})
 			// 单方面删除，不需要给被删除的to推送好友列表
 		}
@@ -124,7 +124,7 @@ func DeleteFriendReq(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"code": 200,
 				"msg":  "删除成功",
-				"data": "新的好友列表发起删除的ToUser",
+				"data": FormatFriendList(rightRelation.To, c),
 			})
 			// 单方面删除，不需要给被删除的from推送好友列表
 		}
@@ -136,27 +136,87 @@ func DeleteFriendReq(c *gin.Context) {
 	}
 }
 
+// TakeBlackReq 某用户发起的删除好友请求，
+// 根据发起拉黑的用户是to还是from来为 status 设 4：from拉黑了to  还是 5：to拉黑了from
+func TakeBlackReq(c *gin.Context) {
+	// 拉黑时并不知道到底 发起拉黑的用户是from还是被拉黑人是from
+	var unknownRecord model.Relation
+	c.ShouldBindJSON(&unknownRecord)
+	// 先判断一下
+	rightRelation, _ := model.GetRightRelationRecord(unknownRecord.From, unknownRecord.To)
+	if unknownRecord.From == rightRelation.From {
+		// 发起拉黑的用户就是  发起好友请求的人（from） status = 4
+		modifyErr := model.ModifyStatus(rightRelation.From, rightRelation.To, 4)
+		if modifyErr != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 2003,
+				"msg":  "拉黑失败",
+			})
+		} else {
+			// 推送给发起拉黑与被拉黑用户双方，因为前端双方都需要这个status进行拦截黑名单发送聊天操作
+			c.JSON(http.StatusOK, gin.H{
+				"code": 200,
+				"msg":  "拉黑成功",
+				"data": FormatFriendList(rightRelation.From, c),
+			})
+			// 推送给被拉黑用户to
+		}
+	} else if unknownRecord.From == rightRelation.To {
+		// 发起拉黑的用户是  一开始接受好友请求的人（to） status = 5
+		modifyErr := model.ModifyStatus(rightRelation.From, rightRelation.To, 5)
+		if modifyErr != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 2003,
+				"msg":  "拉黑失败",
+			})
+		} else {
+			// 推送给发起拉黑与被拉黑用户双方，因为前端双方都需要这个status进行拦截黑名单发送聊天操作
+			c.JSON(http.StatusOK, gin.H{
+				"code": 200,
+				"msg":  "删除成功",
+				"data": FormatFriendList(rightRelation.To, c),
+			})
+			// 推送给被拉黑用户from
+		}
+	} else {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 2004,
+			"msg":  "查询记录失败，不存在该关系记录",
+		})
+	}
+}
+
 // FormatFriendList 格式化获得 friendList ;  userid为当前登录用户
-func FormatFriendList(relationList []model.Relation, userid string) []*Friend {
+func FormatFriendList(userid string, c *gin.Context) []*Friend {
+	relationList, err := model.SelectFriends(userid)
 	var friendList = make([]*Friend, len(relationList))
-	for i, relation := range relationList {
-		if relation.From == userid {
-			// 不要把自己的信息录入 friendList
-			userProfile, _ := model.SelectUser(relation.To)
-			friendList[i] = &Friend{
-				FriendProfile: userProfile,
-				AddTime:       relation.CreatedAt,
-				Status:        relation.Status,
-			}
-		} else if relation.To == userid {
-			// 不要把自己的信息录入 friendList
-			userProfile, _ := model.SelectUser(relation.From)
-			friendList[i] = &Friend{
-				FriendProfile: userProfile,
-				AddTime:       relation.CreatedAt,
-				Status:        relation.Status,
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 2003,
+			"msg":  "获取好友列表失败",
+		})
+	} else {
+		for i, relation := range relationList {
+			if relation.From == userid {
+				// 不要把自己的信息录入 friendList
+				userProfile, _ := model.SelectUser(relation.To)
+				friendList[i] = &Friend{
+					FriendProfile: userProfile,
+					AddTime:       relation.CreatedAt,
+					Status:        relation.Status,
+				}
+			} else if relation.To == userid {
+				// 不要把自己的信息录入 friendList
+				userProfile, _ := model.SelectUser(relation.From)
+				friendList[i] = &Friend{
+					FriendProfile: userProfile,
+					AddTime:       relation.CreatedAt,
+					Status:        relation.Status,
+				}
 			}
 		}
 	}
+
 	return friendList
 }
