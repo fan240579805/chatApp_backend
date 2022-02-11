749299e2f8ac2fc1986e6b40cf6aacd4bef49075
lucafan <lucafan@tencent.com>
2022-02-11 16:12:15 +0800

    feat: 完善添加

--- dao/mysql.go
+++ dao/mysql.go
@@ -10,9 +10,9 @@ var(
 
 func InitMysql() (err error){
 	// win密码6位
-	//dsn:="root:123456@(127.0.0.1:3306)/chat?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn:="root:123456@(127.0.0.1:3306)/chat?charset=utf8mb4&parseTime=True&loc=Local"
 	// mac密码8位
-	dsn:="root:12345678@(127.0.0.1:3306)/chat?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn:="root:12345678@(127.0.0.1:3306)/chat?charset=utf8mb4&parseTime=True&loc=Local"
 	DB,err=gorm.Open("mysql",dsn)
 	if err != nil {
 		return
